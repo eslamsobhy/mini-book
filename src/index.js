4ac97e1b1ede130fe96e6422b75f5d7767eae456
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-06-12 18:47:35 +0200

    improved the presentation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+//CSS
+import "./index.css";
+
 function BookList() {
   return (
-    <section>
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
       <Book />
       <Book />
     </section>
@@ -12,7 +19,7 @@ function BookList() {
 
 const Book = () => {
   return (
-    <article>
+    <article className="book">
       <Image />
       <Title />
       <Author />
@@ -22,12 +29,12 @@ const Book = () => {
 
 const Image = () => (
   <img
-    src="https://images-na.ssl-images-amazon.com/images/I/61FyIO6yVaL._AC_UL302_SR302,200_.jpg"
-    alt="The Wonderful Things You Will Be"
+    src="https://th.bing.com/th/id/OIP.7OUfl03Em5I9haNN4r0AuQHaHp?w=188&h=193&c=7&r=0&o=5&pid=1.7"
+    alt="I Love You to the Moon and Back"
   />
 );
 
-const Title = () => <h1>The Wonderful Things You Will Be</h1>;
-const Author = () => <h4>Emily Winfield Martin</h4>;
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+const Author = () => <h4>Amelia Hepworth</h4>;
 
 ReactDom.render(<BookList />, document.getElementById("root"));
