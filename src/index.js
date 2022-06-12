6ca45c2bef36c223ac0b7ef3163c30048dc2cc9a
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-06-12 23:12:51 +0200

    used props to make it more dynamic

--- src/index.js
+++ src/index.js
@@ -4,6 +4,52 @@ import ReactDom from "react-dom";
 //CSS
 import "./index.css";
 
+// SETUP VARIABLES:
+
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg",
+  alt: "I Love You to the Moon and Back",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth",
+};
+
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/613KCs7szvL._AC_UL604_SR604,400_.jpg",
+  alt: "Ugly Love: A Novel",
+  title: "Ugly Love: A Novel",
+  author: "Colleen Hoover",
+};
+
+const thirdBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51kcX5PpaZL._AC_UL604_SR604,400_.jpg",
+  alt: "The Alchemist, 25th Anniversary: A Fable About Following Your Dream",
+  title: "The Alchemist, 25th Anniversary: A Fable About Following Your Dream",
+  author: "Paulo Coelho",
+};
+
+const fourthBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/71guj5WgRQL._AC_UL604_SR604,400_.jpg",
+  alt: "The Postmodern Pilgrim's Progress: An Allegorical Tale",
+  title: "The Postmodern Pilgrim's Progress: An Allegorical Tale",
+  author: "Kyle Mann",
+};
+
+const fifthBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/71KPMr-rEqL._AC_UL604_SR604,400_.jpg",
+  alt: "Exceptionally Bad Dad Jokes: So frightfully awful.. yet wonderfully spiffing",
+  title:
+    "Exceptionally Bad Dad Jokes: So frightfully awful.. yet wonderfully spiffing",
+  author: "Spiffy McChappy",
+};
+
+const sixthBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/71Q+cIeZWKL._AC_UL604_SR604,400_.jpg",
+  alt: "Scars and Stripes: An Unapologetically American Story of Fighting the Taliban, UFC Warriors, and Myself",
+  title:
+    "Scars and Stripes: An Unapologetically American Story of Fighting the Taliban, UFC Warriors, and Myself",
+  author: "Tim Kennedy",
+};
+
 const Container = () => {
   return (
     <div>
@@ -25,28 +71,52 @@ const Header = () => {
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        alt={firstBook.alt}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        alt={secondBook.alt}
+        author={secondBook.author}
+        title={secondBook.title}
+      />
+      <Book
+        img={thirdBook.img}
+        alt={thirdBook.alt}
+        author={thirdBook.author}
+        title={thirdBook.title}
+      />
+      <Book
+        img={fourthBook.img}
+        alt={fourthBook.alt}
+        author={fourthBook.author}
+        title={fourthBook.title}
+      />
+      <Book
+        img={fifthBook.img}
+        alt={fifthBook.alt}
+        author={fifthBook.author}
+        title={fifthBook.title}
+      />
+      <Book
+        img={sixthBook.img}
+        alt={sixthBook.alt}
+        author={sixthBook.author}
+        title={sixthBook.title}
+      />
     </section>
   );
 }
 
-const author = "Amelia Hepworth";
-const title = "I Love You to the Moon and Back";
-const image =
-  "https://th.bing.com/th/id/OIP.7OUfl03Em5I9haNN4r0AuQHaHp?w=188&h=193&c=7&r=0&o=5&pid=1.7";
-const alt = "I Love You to the Moon and Back";
-
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <img src={image} alt={alt} />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+      <img src={props.img} alt={props.alt} />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
