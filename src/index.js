e46cc9182fc2a514bc03571bbaaf4835d538c801
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-06-12 17:00:36 +0200

    created the book structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const Greeting = () => <h1>Hello world!</h1>;
+function BookList() {
+  return (
+    <section>
+      <Book />
+      <Book />
+    </section>
+  );
+}
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/61FyIO6yVaL._AC_UL302_SR302,200_.jpg"
+    alt="The Wonderful Things You Will Be"
+  />
+);
+
+const Title = () => <h1>The Wonderful Things You Will Be</h1>;
+const Author = () => <h4>Emily Winfield Martin</h4>;
+
+ReactDom.render(<BookList />, document.getElementById("root"));
