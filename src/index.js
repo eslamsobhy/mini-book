58501ccaf3eb4d1f86b76da85e4366e63a7c67c7
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-06-12 21:25:28 +0200

    used JSX javascript

--- src/index.js
+++ src/index.js
@@ -35,35 +35,20 @@ function BookList() {
   );
 }
 
+const author = "Amelia Hepworth";
+const title = "I Love You to the Moon and Back";
+const image =
+  "https://th.bing.com/th/id/OIP.7OUfl03Em5I9haNN4r0AuQHaHp?w=188&h=193&c=7&r=0&o=5&pid=1.7";
+const alt = "I Love You to the Moon and Back";
+
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={image} alt={alt} />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://th.bing.com/th/id/OIP.7OUfl03Em5I9haNN4r0AuQHaHp?w=188&h=193&c=7&r=0&o=5&pid=1.7"
-    alt="I Love You to the Moon and Back"
-  />
-);
-
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-const Author = () => (
-  <h4
-    style={{
-      color: "#617d98",
-      fontSize: "0.75rem",
-      marginTop: "0.25rem",
-      letterSpacing: "5px",
-    }}
-  >
-    Amelia Hepworth
-  </h4>
-);
 
 ReactDom.render(<Container />, document.getElementById("root"));
