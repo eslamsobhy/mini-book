663cfb2887bcb5d033c7ef49848aa0475b976b11
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-06-13 20:15:52 +0200

    key prop & spread operator

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import "./index.css";
 //List of books (objects)
 const books = [
   {
+    id: 0,
     img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg",
     alt: "I Love You to the Moon and Back",
     title: "I Love You to the Moon and Back",
@@ -16,6 +17,7 @@ const books = [
   },
 
   {
+    id: 1,
     img: "https://images-na.ssl-images-amazon.com/images/I/613KCs7szvL._AC_UL604_SR604,400_.jpg",
     alt: "Ugly Love: A Novel",
     title: "Ugly Love: A Novel",
@@ -23,6 +25,7 @@ const books = [
   },
 
   {
+    id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/51kcX5PpaZL._AC_UL604_SR604,400_.jpg",
     alt: "The Alchemist, 25th Anniversary: A Fable About Following Your Dream",
     title:
@@ -31,6 +34,7 @@ const books = [
   },
 
   {
+    id: 3,
     img: "https://images-na.ssl-images-amazon.com/images/I/71guj5WgRQL._AC_UL604_SR604,400_.jpg",
     alt: "The Postmodern Pilgrim's Progress: An Allegorical Tale",
     title: "The Postmodern Pilgrim's Progress: An Allegorical Tale",
@@ -38,6 +42,7 @@ const books = [
   },
 
   {
+    id: 4,
     img: "https://images-na.ssl-images-amazon.com/images/I/71KPMr-rEqL._AC_UL604_SR604,400_.jpg",
     alt: "Exceptionally Bad Dad Jokes: So frightfully awful.. yet wonderfully spiffing",
     title:
@@ -46,11 +51,36 @@ const books = [
   },
 
   {
+    id: 5,
     img: "https://images-na.ssl-images-amazon.com/images/I/71Q+cIeZWKL._AC_UL604_SR604,400_.jpg",
     alt: "Scars and Stripes: An Unapologetically American Story of Fighting the Taliban, UFC Warriors, and Myself",
     title:
       "Scars and Stripes: An Unapologetically American Story of Fighting the Taliban, UFC Warriors, and Myself",
     author: "Tim Kennedy",
+  },
+
+  {
+    id: 6,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL604_SR604,400_.jpg",
+    alt: "The 48 Laws of Power",
+    title: "The 48 Laws of Power",
+    author: "Robert Greene",
+  },
+
+  {
+    id: 7,
+    img: "https://images-na.ssl-images-amazon.com/images/I/617uZq23IPL._AC_UL604_SR604,400_.jpg",
+    alt: "Reminders of Him: A Novel",
+    title: "Reminders of Him: A Novel",
+    author: "Colleen Hoover",
+  },
+
+  {
+    id: 8,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71ZVzeVj1RL._AC_UL604_SR604,400_.jpg",
+    alt: "Sparring Partners (Jake Brigance)",
+    title: "Sparring Partners (Jake Brigance)",
+    author: "John Grisham",
   },
 ];
 
@@ -76,15 +106,14 @@ function BookList() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        console.log(book);
-        return <Book book={book}></Book>;
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
 
 const Book = (props) => {
-  const { img, title, author, alt } = props.book;
+  const { img, title, author, alt } = props;
   return (
     <article className="book">
       <img src={img} alt={alt} />
