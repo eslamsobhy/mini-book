da478661428b99e6fcbbdb9d39230ef4380b027c
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-06-14 16:54:05 +0200

    fixed the extra-whitespaces problem

--- src/index.js
+++ src/index.js
@@ -117,7 +117,7 @@ const Book = (props) => {
   return (
     <article className="book">
       <img src={img} alt={alt} />
-      <h1>{title}</h1>
+      <h1 title={title}>{title}</h1>
       <h4>{author}</h4>
     </article>
   );
